f462fbcf66d7311f06b4d9810f085fb95d5473d1
SunnyStorm254 <mercykendagor@gmail.com>
2022-09-13 01:52:35 -0700

    L

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * File: 4 -puts.c
+ */
+
+#include<studio.h>
+
+/**
+ *main - Prints "Programing is like builidng a multilingual puzzle, followed by a new line.
+Return: Always 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	Return(0)
+}
